cde469fa754ba5276cca541d665fcde965f11878
igama <gaminhaigor@gmail.com>
2023-10-24 19:37:43 -0300

    Feat: new functions

--- The Right Libft/ft_strlen.c
+++ The Right Libft/ft_strlen.c
@@ -6,13 +6,13 @@
 /*   By: igama <igama@student.42.rio>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 16:54:41 by igama             #+#    #+#             */
-/*   Updated: 2023/10/21 21:16:44 by igama            ###   ########.fr       */
+/*   Updated: 2023/10/24 17:59:07 by igama            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlen(const char	*str)
+size_t	ft_strlen(const char *str)
 {
 	size_t	c;
 
