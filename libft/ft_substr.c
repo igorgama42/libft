911d672856c4763ec9ccaa222533b8597621a61c
igama <igama@student.42.rio>
2023-11-11 20:22:30 -0300

    Substr corrected!

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: igama <igama@student.42.rio>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 19:34:31 by igama             #+#    #+#             */
-/*   Updated: 2023/11/11 19:25:11 by igama            ###   ########.fr       */
+/*   Updated: 2023/11/11 20:19:30 by igama            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	size_t	j;
 	char	*str;
 
+	if (!s)
+		return (NULL);
 	str = (char *)malloc(sizeof(*s) * (len + 1));
 	if (str == 0)
 		return (NULL);
@@ -35,26 +37,3 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	str[j] = 0;
 	return (str);
 }
-
-/*char	*ft_substr(char const *s, unsigned int start, size_t len)
-{
-	size_t		i;
-	size_t		j;
-	char		*str;
-
-	if (!s)
-		return (0);
-	str = (char *)malloc(len + 1);
-	if (!str)
-		return (0);
-	i = start;
-	j = 0;
-	while (i < ft_strlen(s) && j < len)
-	{
-		str[j]= s[i];
-		i++;
-		start++;
-	}
-	str[j] = '\0';
-	return (str);
-}*/
