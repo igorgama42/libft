98591048b14e8c189cef0b47cd57af23b558c514
igama <gaminhaigor@gmail.com>
2023-11-12 16:17:30 -0300

    FINAL WORK!

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: igama <igama@student.42.rio>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 15:06:46 by igama             #+#    #+#             */
-/*   Updated: 2023/11/11 01:17:13 by igama            ###   ########.fr       */
+/*   Updated: 2023/11/12 15:35:43 by igama            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,28 +29,28 @@ static int	ft_count_words(const char *str, char separator)
 
 char	**ft_split(char const *s, char c)
 {
-	char	**new_array;
+	char	**array;
 	int		word_num;
 	int		i;
 	int		start;
 
 	if (!s)
 		return (NULL);
-	new_array = ft_calloc(ft_count_words(s, c) + 1, sizeof(char *));
-	if (!new_array)
+	array = ft_calloc(ft_count_words(s, c) + 1, sizeof(char *));
+	if (!array)
 		return (NULL);
 	i = 0;
 	word_num = 0;
 	while (s[i])
 	{
-		while (s[i] != 0 && s[i] == c)
+		while (s[i] && s[i] == c)
 			i++;
 		start = i;
-		while (s[i] != 0 && s[i] != c)
+		while (s[i] && s[i] != c)
 			i++;
 		if (word_num < ft_count_words(s, c))
-			new_array[word_num] = ft_substr(s, start, i - start);
+			array[word_num] = ft_substr(s, start, i - start);
 		word_num++;
 	}
-	return (new_array);
+	return (array);
 }
