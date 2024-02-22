5647a3023b4b814a964a2ab560ec6b267efca001
igorgama42 <148505232+igorgama42@users.noreply.github.com>
2024-02-22 14:25:33 -0300

    Update ft_strjoin.c

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -14,21 +14,28 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	char	*str;
-	int		len_sum;
-	int		len_s1;
-	int		len_s2;
+		char			*str;
+	unsigned int	len;
+	unsigned int	i;
+	unsigned int	j;
 
 	if (!s1 || !s2)
 		return (NULL);
-	len_s1 = ft_strlen(s1);
-	len_s2 = ft_strlen(s2);
-	len_sum = len_s1 + len_s2 + 1;
-	str = malloc(sizeof(char) * (len_sum));
+	len = ft_strlen(s1) + ft_strlen(s2);
+	i = 0;
+	j = 0;
+	str = malloc(len * sizeof(char) + 1);
 	if (!str)
 		return (NULL);
-	ft_memcpy(str, s1, len_s1);
-	ft_memcpy(str + len_s1, s2, len_s2);
-	str[len_sum - 1] = '\0';
+	while (s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		str[i++] = s2[j++];
+	}
+	str[i] = '\0';
 	return (str);
 }
